37e7c20fb38770e794091b263410a43d1370dba2
KishanPrajapati79 <prajapatikishan.kp2002@gmail.com>
2024-02-26 11:32:09 +0530

    appspec and container files updated

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull kishanpra/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 kishanpra/simple-python-flask-app
