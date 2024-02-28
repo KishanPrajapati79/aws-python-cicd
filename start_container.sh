47e2869554d88059f9a793b49223716db1510ffe
KishanPrajapati79 <prajapatikishan.kp2002@gmail.com>
2024-02-28 16:52:45 +0530

    updated

--- start_container.sh
+++ start_container.sh
@@ -6,4 +6,4 @@ docker pull kishanpra/simple-python-flask-app
 
 # Run the Docker image as a container
 # docker run -d -p 5000:5000 kishanpra/simple-python-flask-app
-docker run -d -p 6000:5000 kishanpra/simple-python-flask-app
+docker run -d -p 5000:5000 kishanpra/simple-python-flask-app
